fe45bf3b67c0e3ce3ef043c8b79c9f76f4c86ef8
Daniel-Andarge <andargedaniel90@gmail.com>
2024-05-07 00:05:59 +0300

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The purpose of this report is to analyze the telecommunication dataset of TellCo, an existing mobile service provider in the Republic of Pefkakia. The goal is to identify opportunities for growth and provide a recommendation on whether TellCo is worth buying or selling.',
+    author='Daniel Andarge',
+    license='',
+)
