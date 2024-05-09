afc0f6784183940c326d20d52ca03926c04c3974
Daniel-Andarge <andargedaniel90@gmail.com>
2024-05-09 21:37:27 +0300

    Notebook

--- src/analysis/user_overview/users_behavior_on_apps.py
+++ src/analysis/user_overview/users_behavior_on_apps.py
@@ -1,23 +1,33 @@
 import pandas as pd
 
 
-def aggregate_user_behavior(xdr_data):
-    # Load the xDR session data into a pandas DataFrame
-    df = pd.DataFrame(xdr_data)
+import pandas as pd
 
+""" def aggregate_user_behavior(df):
     # Group the data by IMSI and aggregate the information
-    grouped_data = df.groupby('IMSI')['Session Duration'].count().reset_index()
-    grouped_data.rename(columns={'Session Duration': 'Number of xDR sessions'}, inplace=True)
+    df['Dur. (ms)'] = df['Dur. (ms)'].astype(float)
+    grouped_data = df.groupby('IMSI').agg({'Dur. (ms)': 'count'})
+    grouped_data.rename(columns={'Dur. (ms)': 'Number of xDR sessions'}, inplace=True)
 
-    grouped_data['Session duration'] = df.groupby('IMSI')['Session Duration'].sum()
+    grouped_data['Session duration'] = df.groupby('IMSI')['Dur. (ms)'].sum()
     grouped_data['Total Download (DL) data'] = df.filter(like='DL').sum(axis=1)
     grouped_data['Total Upload (UL) data'] = df.filter(like='UL').sum(axis=1)
     grouped_data['Total Data Volume'] = df.filter(like='Bytes').sum(axis=1)
 
+    return grouped_data """
+
+def aggregate_user_behavior(df):
+    # Group the data by IMSI and aggregate the information
+    df['Dur. (ms)'] = df['Dur. (ms)'].astype(float)
+    agg_functions = {
+        'Dur. (ms)': 'count',
+        'Session duration': 'sum',
+        'Total Download (DL) data': 'sum',
+        'Total Upload (UL) data': 'sum',
+        'Total Data Volume': 'sum'
+    }
+    grouped_data = df.groupby('IMSI').agg(agg_functions)
+    grouped_data.rename(columns={'Dur. (ms)': 'Number of xDR sessions'}, inplace=True)
+
     return grouped_data
 
-# Aggregate user behavior
-user_behavior = aggregate_user_behavior(xdr_data)
-
-# Print the aggregated information
-print(user_behavior)
